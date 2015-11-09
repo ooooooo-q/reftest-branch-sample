af616c97b81f65beef0a1f02ab56d8a637ad5916
ooooooo-q <ooooooo-q@users.noreply.github.com>
2015-11-10 01:16:44 +0900

    gitfix

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@ mkdir -p copy_dir/git
 rsync -ar --delete --exclude "/copy_dir" --exclude "/node_modules" . ./copy_dir/git
 cd copy_dir/git
 git stash
-git checkout master
+git fetch --prune
+git checkout 7d266d52aaa409eef0b6cb59c8ee707db16afd72
 cd -
 pwd
 node reftest.js
